150a9cdbd32f0da505ae643477602442f5b2cafc
rylandgrounds <rylandgrounds@gmail.com>
2020-08-04 10:52:37 -0500

    added user signup and login features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:id] = @user.id
+            redirect_to 'users/show'
+        else
+            redirect_to 'users/new'
+        end
+    end
+    
+    def show
+        current_user
+    
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/' => 'sessions#welcome'
+  get '/login' => "sessions#new"
+  post '/login' => "sessions#create"
+  delete 'logout' => "sessions#logout"
+  get '/signup' => "users#new"
+  post '/signup' => "users#create"
+  get '/auth/:provider/callback', to: 'sessions#create'
 end

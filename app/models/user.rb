abf55e02bee6da055aa9260132a12b9bfe181a10
rylandgrounds <rylandgrounds@gmail.com>
2020-08-04 11:23:08 -0500

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username , presence: true,  uniqueness: true
+
+    def self.find_or_create_by_omniauth(auth_hash)
+        self.where(username: auth_hash.info.username).first_or_create do |user|
+            user.password = SecureRandom.hex
+        end
+    end
 end

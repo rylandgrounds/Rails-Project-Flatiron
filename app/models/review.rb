f6fd018dc0328c9022999919f3ab5ba9afe0faeb
rylandgrounds <rylandgrounds@gmail.com>
2020-08-11 17:19:10 -0500

    fixed scope issue with review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
     belongs_to :book
     belongs_to :user
     validates :content, presence: true
-    validates :user_id , presence: true,  uniqueness: {scope: book_id, message: "Only one review per book!"}
+    validates :user_id , presence: true,  uniqueness: {scope: :book_id, message: "Only one review per book!"}
     
     def creator
        @creator = User.find_by(id: self.user_id)
